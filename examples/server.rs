97d7195905e4d3caf0a37c7015947d14b91cbf62
Stefan Müller <s.mueller@it.kls-glt.de>
2016-06-24 17:24:29 +0200

    Version ohne Referenzen
    Fehler 'move occurs because  has type , which does not implement the  trait'

---
+++ examples/server.rs
@@ -0,0 +1,19 @@
+extern crate playground;
+
+use playground::{Server, Sensor, Module};
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let mut server = Server::new();
+    let module1 = Module::new();
+    server.modules.push(module1);
+
+    loop {
+        thread::spawn(move || {
+            // server.update_sensors();
+        });
+
+        thread::sleep(Duration::from_millis(1000));
+    }
+}
